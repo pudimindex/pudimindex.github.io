755c230654d003355a1b6d04f02257372769d1ec
sabrinabgbc <sabrinabgbc@email.com>
2017-07-29 22:44:17 -0300

    cnd e estilos

---
+++ templates/footer.php
@@ -0,0 +1,12 @@
+    	</main>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="assets/dist/bootstrap.min.js"></script>
+        <script src="assets/dist/angular.min.js"></script>
+        <script src="assets/js/main.js"></script>
+        <footer>
+            <nav class="menu-footer">
+                
+            </nav>
+        </footer>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="title" content="">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="">
+	<link rel="stylesheet" href="assets/dist/font-awesome.min.css"/>
+	<link rel="stylesheet" href="assets/dist/bootstrap.min.css"/>
+	<link rel="stylesheet" href="assets/css/main.css"/>
+	<link rel="stylesheet" href="assets/css/style.css"/>
+	<!--[if lt IE 7]>
+		<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE7.js"></script>
+	<![endif]-->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <script>
+			var e = ("abbr,article,aside,audio,canvas,datalist,details," + "figure,footer,header,hgroup,mark,menu,meter,nav,output," + "progress,section,time,video").split(',');
+			for (var i = 0; i < e.length; i++) {
+			     document.createElement(e[i]);
+		    }
+	    </script>
+	<![endif] -->
+</head>
+<header>
+	<nav class="header-menu">
+		
+	</nav>
+</header>
+<main class="main">
